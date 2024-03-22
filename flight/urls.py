7e744ce58c256394e1393411e93467accb32bb58
jinheumkim <kjjppwl510@naver.com>
2024-03-22 17:05:01 +0900

    3/22 commit

--- flight/urls.py
+++ flight/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Main
+from .views import Main,Search_flight
 
 urlpatterns = [
     path('main', Main.as_view()),
+    path('search', Search_flight.as_view()),
+    
 ]
--- flight/views.py
+++ flight/views.py
@@ -13,3 +13,11 @@ class Main(APIView):
         user_login = User.objects.filter(email = email).exists()
         
         return render(request, "flight/main.html", context = dict(user = user, user_login=user_login))
+    
+class Search_flight(APIView):
+    def get(self,request):
+        email = request.session.get('email',None) 
+        user = User.objects.filter(email = email).first()
+        user_login = User.objects.filter(email = email).exists()
+        
+        return render(request, "flight/search.html", context = dict(user = user, user_login = user_login))

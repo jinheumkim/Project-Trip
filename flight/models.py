30f33be0ebc6fbb45a62a07760d533bf3e332d53
jinheumkim <kjjppwl510@naver.com>
2024-03-22 23:18:32 +0900

    3/22 commit

--- flight/models.py
+++ flight/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class FlightSchedule(models.Model):
+    flight_code = models.CharField(max_length = 45)
+    departure_date = models.DateField()
+    arrival_date = models.DateField()
+    departure_time = models.TimeField()
+    arrival_time = models.TimeField()
+    duration_time = models.TimeField()
+    departure_airport = models.ForeignKey('Airport', related_name='departure_airport', on_delete=models.CASCADE)
+    arrival_airport = models.ForeignKey('Airport', related_name='arrival_airport', on_delete=models.CASCADE)
+    airline = models.ForeignKey('Airline', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'flight_schedules'
+        
+class Airline(models.Model):
+    name = models.CharField(max_length = 300)
+    image = models.TextField()
+    
+    class Meta:
+        db_table = 'airlines'
+
+class Airport(models.Model):
+    name = models.CharField(max_length=400)
+    code = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'airports'
+        
+class FlightPrice(models.Model):
+    price           = models.DecimalField(max_digits=30, decimal_places=2)
+    remaining_seat  = models.IntegerField()
+    flight_schedule = models.ForeignKey('FlightSchedule', on_delete=models.CASCADE, related_name='flight_prices')
+    status          = models.ForeignKey('FlightStatus', on_delete=models.CASCADE, default=1) 
+
+    class Meta:
+        db_table = 'flight_prices'
+        
+class FlightStatus(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'flight_statuses'

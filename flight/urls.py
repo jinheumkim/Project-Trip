240b27c831a706c66f60f7fb76cb1ce37bc14a26
jinheumkim <kjjppwl510@naver.com>
2024-04-03 17:39:27 +0900

    4/3 commit

--- flight/urls.py
+++ flight/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Main, Search
+from .views import Main, Search, SaveData
 
 urlpatterns = [
     path('main', Main.as_view()),
     path('search/', Search.as_view()),
+    path('search_data/', SaveData.as_view())
 ]
--- flight/views.py
+++ flight/views.py
@@ -16,151 +16,103 @@ class Main(APIView):
         
         return render(request, "flight/main.html", context = dict(user = user, user_login=user_login))
     
-
-
-# class Search(APIView):
-#     def get(self, request):
-            
-#         email = request.session.get('email',None)
-                    
-#         user = User.objects.filter(email = email).first()
-#         user_login = User.objects.filter(email = email).exists()
+class SaveData(APIView):
+    def post(self, request):
+        departure_airport = request.data.get('departure_airport')
+        arrival_airport = request.data.get('arrival_airport')
+        border_count = request.data.get('border_count')
+        departure_date = request.data.get('departure_date')
+        arrival_date = request.data.get('arrival_date')
+        
+        departure_flight_code = request.data.get('departure_flight_code')
         
         
-#         departure_airport = request.GET.get('key1')
-#         arrival_airport = request.GET.get('key2')
-#         departure_date = request.GET.get('key3')
-#         arrival_date = request.GET.get('key4')
-#         border_count = request.GET.get('key5')
-        
-#         departure_airport_id = Airport.objects.filter(name__icontains=departure_airport).first()
-#         arrival_airport_id = Airport.objects.filter(name__icontains=arrival_airport).first()
-        
-#         flight_object_list = FlightSchedule.objects.all()
-        
-#         departure_list = []
-#         arrival_list = []        
-                      
-            
-#         departure = FlightSchedule.objects.filter(departure_airport = departure_airport_id.id, departure_date = departure_date)
-#         departure_count = FlightSchedule.objects.filter(departure_airport = departure_airport_id.id, departure_date = departure_date).count()
-#         arrival = FlightSchedule.objects.filter(arrival_airport = arrival_airport_id.id, arrival_date = arrival_date)
-#         arrival_count = FlightSchedule.objects.filter(arrival_airport = arrival_airport_id.id, arrival_date = arrival_date).count()
-        
-#         for search in departure:
-#             departure_list.append(dict(departure_airport = search.departure_airport,
-#                                     departure_date = search.departure_date,
-#                                     arrival_airport = search.arrival_airport,
-#                                     arrival_date = search.arrival_date,
-#                                     airline = search.airline,
-#                                     flight_code = search.flight_code,
-#                                     departure_time = search.departure_time,
-#                                     arrival_time = search.arrival_time,
-#                                     departure_airport_code = departure_airport_id.code,
-#                                     arrival_airport_code = arrival_airport_id.code,
-#                                     id = search.id
-#                                     ))
-                
-#         for search in arrival:
-#             arrival_list.append(dict(departure_airport = search.departure_airport,
-#                                     departure_date = search.departure_date,
-#                                     arrival_airport = search.arrival_airport,
-#                                     arrival_date = search.arrival_date,
-#                                     airline = search.airline,
-#                                     flight_code = search.flight_code,
-#                                     departure_time = search.departure_time,
-#                                     arrival_time = search.arrival_time,
-#                                     departure_airport_code = departure_airport_id.code,
-#                                     arrival_airport_code = arrival_airport_id.code,
-#                                     id = search.id))
-            
-#             return render(request, "flight/search.html", context = dict(user=user, email=email,
-#                                                                         user_login = user_login,
-#                                                                         departure = departure_list,
-#                                                                         arrival = arrival_list,
-#                                                                         departure_airport = departure_airport,
-#                                                                         arrival_airport = arrival_airport,
-#                                                                         departure_date = departure_date,
-#                                                                         arrival_date = arrival_date,
-#                                                                         border_count = border_count,
-#                                                                         departure_count = departure_count,
-#                                                                         arrival_count = arrival_count,
-#                                                                         flight_object_list = flight_object_list
-#                                                                         ))
-
+        request.session['departure_airport'] = departure_airport
+        request.session['arrival_airport'] = arrival_airport
+        request.session['border_count'] = border_count
+        request.session['departure_date'] = departure_date
+        request.session['arrival_date'] = arrival_date
+        request.session['departure_flight_code'] = arrival_date
+        request.session['departure_flight_code'] = departure_flight_code
+        return Response(status=200)
+    
 class Search(APIView):
-    def get(self,request):
+    def get(self, request):
         email = request.session.get('email',None)
                     
         user = User.objects.filter(email = email).first()
         user_login = User.objects.filter(email = email).exists()
         
-        flight_object_list = FlightSchedule.objects.all()
+        departure_airport =  request.session.get('departure_airport',None)
+        arrival_airport =  request.session.get('arrival_airport',None)
+        border_count =  request.session.get('border_count',None)
+        departure_date =  request.session.get('departure_date',None)
+        arrival_date =  request.session.get('arrival_date',None)
+        departure_flight_code = request.session.get('departure_flight_code', None)
         
-        return render(request, "flight/search.html", context = dict(user = user,
-                                                                    user_login = user_login,
-                                                                    flight_object_list = flight_object_list
-                                                                    ))
         
-    def post(self,request):
+        departure_dates = FlightSchedule.objects.filter(departure_date__icontains = departure_date)
+        departure_count = FlightSchedule.objects.filter(departure_date__icontains = departure_date).count()
+        arrival_dates = FlightSchedule.objects.filter(arrival_date__icontains = arrival_date)
+        arrival_count = FlightSchedule.objects.filter(arrival_date__icontains = arrival_date).count()
         
-        departure_airport = request.GET.get('key1')
-        arrival_airport = request.GET.get('key2')
-        departure_date = request.GET.get('key3')
-        arrival_date = request.GET.get('key4')
-        border_count = request.GET.get('key5')
+        departure_select = FlightSchedule.objects.filter(flight_code = departure_flight_code)
+        departure_selecting = FlightSchedule.objects.filter(flight_code = departure_flight_code).exists()
+         
+            
+        departure_select_list = []
         
-        departure_airport_id = Airport.objects.filter(name__icontains=departure_airport).first()
-        arrival_airport_id = Airport.objects.filter(name__icontains=arrival_airport).first()
-        
+        if departure_select is not None:
+            for select in departure_select:
+                departure_select_list.append(dict(id = select.id,
+                                                  flight_code = select.flight_code,
+                                                  departure_time = select.departure_time,
+                                                  departure_airport = select.departure_airport,
+                                                  departure_date = select.departure_date,
+                                                  arrival_time = select.arrival_time,
+                                                  arrival_airport = select.arrival_airport,
+                                                  airline_image = select.airline.image,
+                                                  departure_airport_code = select.departure_airport.code,
+                                                  arrival_airport_code = select.arrival_airport.code))
+                
+                
+        arrival_list = []
+        if departure_selecting == True:
+            for arrival in arrival_dates:
+                arrival_list.append(dict(flight_code = arrival.flight_code,
+                                    id = arrival.id,
+                                    departure_time = arrival.departure_time,
+                                    airline = arrival.airline,
+                                    arrival_airport = arrival.arrival_airport,
+                                    arrival_date = arrival.arrival_date,
+                                    arrival_time = arrival.arrival_time,
+                                    departure_airport = arrival.departure_airport,
+                                    ))
         
         departure_list = []
-        arrival_list = []        
-                      
-            
-        departure = FlightSchedule.objects.filter(departure_airport = departure_airport_id.id, departure_date = departure_date)
-        departure_count = FlightSchedule.objects.filter(departure_airport = departure_airport_id.id, departure_date = departure_date).count()
-        arrival = FlightSchedule.objects.filter(arrival_airport = arrival_airport_id.id, arrival_date = arrival_date)
-        arrival_count = FlightSchedule.objects.filter(arrival_airport = arrival_airport_id.id, arrival_date = arrival_date).count()
-        
-        for search in departure:
-            departure_list.append(dict(departure_airport = search.departure_airport,
-                                    departure_date = search.departure_date,
-                                    arrival_airport = search.arrival_airport,
-                                    arrival_date = search.arrival_date,
-                                    airline = search.airline,
-                                    flight_code = search.flight_code,
-                                    departure_time = search.departure_time,
-                                    arrival_time = search.arrival_time,
-                                    departure_airport_code = departure_airport_id.code,
-                                    arrival_airport_code = arrival_airport_id.code,
-                                    id = search.id
+        for departure in departure_dates:
+            departure_list.append(dict(flight_code = departure.flight_code,
+                                    id = departure.id,
+                                    departure_time = departure.departure_time,
+                                    airline = departure.airline,
+                                    departure_airport = departure.departure_airport,
+                                    departure_date = departure.departure_date,
+                                    arrival_time = departure.arrival_time,
+                                    arrival_airport = departure.arrival_airport,
                                     ))
-                
-        for search in arrival:
-            arrival_list.append(dict(departure_airport = search.departure_airport,
-                                    departure_date = search.departure_date,
-                                    arrival_airport = search.arrival_airport,
-                                    arrival_date = search.arrival_date,
-                                    airline = search.airline,
-                                    flight_code = search.flight_code,
-                                    departure_time = search.departure_time,
-                                    arrival_time = search.arrival_time,
-                                    departure_airport_code = departure_airport_id.code,
-                                    arrival_airport_code = arrival_airport_id.code,
-                                    id = search.id))
+            
         
         
-        
-        return render(request,"flight/search.html", context = dict(     departure = departure_list,
-                                                                        arrival = arrival_list,
-                                                                        departure_airport = departure_airport,
-                                                                        arrival_airport = arrival_airport,
-                                                                        departure_date = departure_date,
-                                                                        arrival_date = arrival_date,
-                                                                        border_count = border_count,
-                                                                        departure_count = departure_count,
-                                                                        arrival_count = arrival_count,
-                                                                        flight_object_list = flight_object_list))
-        
-        
+        return render(request, 'flight/search.html',context = dict(departure_airport = departure_airport,
+                                                                   arrival_airport = arrival_airport,
+                                                                   border_count = border_count,
+                                                                   departure_date = departure_date,
+                                                                   arrival_date = arrival_date,
+                                                                   user = user, user_login = user_login,
+                                                                   departures = departure_list,
+                                                                   departure_count = departure_count,
+                                                                   departure_select = departure_select_list,
+                                                                   departure_selecting = departure_selecting,
+                                                                   arrivals = arrival_list,
+                                                                   arrival_count = arrival_count
+                                                                   ))
